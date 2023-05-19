9f3fd7a0c1befb3aa3ba05e245d8383f80212512
Oliver Winter <owinter86@gmail.com>
2023-05-19 10:17:51 +0200

    fix: update exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: ["universe/native", "universe/shared/typescript-analysis", "plugin:prettier/recommended"],
   env: { jest: true, node: true },
-  ignorePatterns: ["**/generated/**"],
+  ignorePatterns: ["**/generated/**", "lib/**/**"],
   overrides: [
     {
       files: ["*.ts", "*.tsx", "*.d.ts"],
--- src/primitives/Box/AnimatedBox.tsx
+++ src/primitives/Box/AnimatedBox.tsx
@@ -6,16 +6,18 @@ import { SafeAreaViewProps } from "react-native-safe-area-context";
 import { RemoveStyle } from "tools/useStyle";
 
 // Make sure to use the default style prop to allow any animation
-export type BoxProps = RemoveStyle<BoxTokens> & SafeAreaViewProps;
-export const AnimatedBox = forwardRef<Animated.View, BoxProps>(({ style, children, ...props }, ref) => {
-  const { tokenStyles, paddingValues, ...rest } = useResolveBoxTokens(props);
-  const color = useContext(BackgroundContext);
+export type AnimatedBoxProps = RemoveStyle<BoxTokens> & SafeAreaViewProps;
+export const AnimatedBox = forwardRef<Animated.View, AnimatedBoxProps>(
+  ({ style, children, ...props }, ref) => {
+    const { tokenStyles, paddingValues, ...rest } = useResolveBoxTokens(props);
+    const color = useContext(BackgroundContext);
 
-  return (
-    <BackgroundContext.Provider value={tokenStyles.backgroundColor ?? color}>
-      <Animated.View ref={ref} style={[tokenStyles, paddingValues, style]} {...rest}>
-        {children}
-      </Animated.View>
-    </BackgroundContext.Provider>
-  );
-});
+    return (
+      <BackgroundContext.Provider value={tokenStyles.backgroundColor ?? color}>
+        <Animated.View ref={ref} style={[tokenStyles, paddingValues, style]} {...rest}>
+          {children}
+        </Animated.View>
+      </BackgroundContext.Provider>
+    );
+  }
+);
